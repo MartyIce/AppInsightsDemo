749d6519a48d86b2eda86196ac5d89a4ee80b9de
Marty Mavis <marty.mavis@gmail.com>
2018-08-02 13:51:54 -0500

    MVC enabled

--- AppInsightsDemo/Startup.cs
+++ AppInsightsDemo/Startup.cs
@@ -11,10 +11,13 @@ namespace AppInsightsDemo
 {
     public class Startup
     {
+        // https://codeburst.io/how-to-use-webpack-in-asp-net-core-projects-a-basic-react-template-sample-25a3681a5fc2
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +27,14 @@ namespace AppInsightsDemo
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+                {
+                    routes.MapRoute(
+                        name: "default",
+                        template: "{controller=Home}/{action=Index}/{id?}");
+                }                                         
+            );
         }
     }
 }
